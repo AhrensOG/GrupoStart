b0b6f923578c7bb399f8ed07f014cec49bfb5310
AhrensOG <ahrensgabriel09@gmail.com>
2023-06-10 18:31:10 -0300

    Fix some warns in the html structure

--- components/carousel/Card.jsx
+++ components/carousel/Card.jsx
@@ -13,35 +13,35 @@ const Card = ({
   description = data.description,
 }) => {
   return (
-    <div class="sm:w-[350px] w-[290px] bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 hover:shadow-black/70 hover:shadow-xl hover:-translate-y-2 ease-in duration-200">
+    <div className="sm:w-[350px] w-[290px] bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 hover:shadow-black/70 hover:shadow-xl hover:-translate-y-2 ease-in duration-200">
       <a href="#">
-        <img class="rounded-t-lg" src={`${img}`} alt="" />
+        <img className="rounded-t-lg" src={`${img}`} alt="" />
       </a>
-      <div class="p-5 text-center sm:w-[350px] w-[290px]">
+      <div className="p-5 text-center sm:w-[350px] w-[290px]">
         <a href="#">
-          <h5 class="mb-2 text-2xl text-start font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-2 text-2xl text-start font-bold tracking-tight text-gray-900 dark:text-white">
             {title}
           </h5>
         </a>
-        <p class="mb-3 text-start font-normal text-gray-700 dark:text-gray-400">
+        <p className="mb-3 text-start font-normal text-gray-700 dark:text-gray-400">
           {description}
         </p>
         <a
           href="#"
-          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Leer Mas
           <svg
             aria-hidden="true"
-            class="w-4 h-4 ml-2 -mr-1"
+            className="w-4 h-4 ml-2 -mr-1"
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
         </a>
