d27467672c85daf1d7c452533b6ecfae06651352
AhrensOG <ahrensgabriel09@gmail.com>
2023-06-12 18:56:04 -0300

    Change Titles Head of pages

--- pages/empresas/index.js
+++ pages/empresas/index.js
@@ -7,12 +7,16 @@ import SearchBar from '../../components/search/SearchBar'
 import ListCompanyCards from '../../components/listCompanyCards/ListCompanyCards'
 import { useRouter } from 'next/router'
 import Wsp from '../../components/whatsapp/Wsp'
+import Head from 'next/head'
 
 const Empresas = () => {
   const { asPath } = useRouter() 
   const navbarButtons = asPath === '/empresas'
   return (
     <div>
+      <Head>
+        <title>GrupoStart</title>
+      </Head>
       <NavBar servicesDropdown={!navbarButtons}/>
       <Carousel autoSlide={true} autoSlideInterval={5000}/>
       <SimpleSlider/>
--- pages/servicios/index.js
+++ pages/servicios/index.js
@@ -3,10 +3,14 @@ import NavBar from '../../components/navBar/NavBar'
 import Footer from '../../components/footer/Footer'
 import Image from 'next/image'
 import Wsp from '../../components/whatsapp/Wsp'
+import Head from 'next/head'
 
 const Servicios = () => {
   return (
     <div>
+      <Head>
+        <title>GrupoStart</title>
+      </Head>
       <NavBar/>
       <div className='pt-[80px]'>
         <Image src='/services/1.png' priority={true} alt='img' width={10000} height={10000} className='w-full h-full'/>
