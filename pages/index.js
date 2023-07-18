502d70c4d55740d25360a6619564f3b6c1e0a4b4
AhrensOG <ahrensgabriel09@gmail.com>
2023-07-18 19:56:15 -0300

    Change Navbar Style

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import Services from "../components/services/Services";
 import Wsp from "../components/whatsapp/Wsp";
 import Head from "next/head";
 import HeroV2 from "../components/hero/HeroV2";
+import NavBarV2 from "../components/navBar/NavBarV2";
 
 export default function Home() { 
   const { asPath } = useRouter() 
@@ -17,9 +18,10 @@ export default function Home() {
       <Head>
         <title>GrupoStart</title>
       </Head>
-      <NavBar home={!navbarButtons} servicesDropdown={!navbarButtons} />
+      {/* <NavBar home={!navbarButtons} servicesDropdown={!navbarButtons} /> */}
+      <NavBarV2 home={!navbarButtons} servicesDropdown={!navbarButtons} />
       <HeroV2/>
-      <About/>
+      {/* <About/> */}
       <Services/>
       <Contact/>
       <Footer/>
