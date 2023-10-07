295b71fbde72e6d20b1016bcf24034f5da7782a5
AhrensOG <ahrensgabriel09@gmail.com>
2023-10-07 12:07:11 -0300

    Fix SERVER_BASE_URL  error - 8

--- db/config/config.js
+++ db/config/config.js
@@ -1,7 +1,4 @@
 require('dotenv').config()
-
-console.log(process.env.DB_USERNAME, process.env.DB_PASSWORD, process.env.DB_NAME, process.env.DB_HOSTNAME, process.env.DB_PORT)
-
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
