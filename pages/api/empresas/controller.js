d4bd177bcf3adaf740e5724de8df89d3656f4fdb
AhrensOG <ahrensgabriel09@gmail.com>
2023-10-07 11:51:18 -0300

    Fix SERVER_BASE_URL  error - 5

--- pages/api/empresas/controller.js
+++ pages/api/empresas/controller.js
@@ -1,4 +1,4 @@
-import Empresas from "../../../db/models/empresas"
+// import Empresas from "../../../db/models/empresas"
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
@@ -8,70 +8,70 @@ export default async function handler(req, res) {
     } catch (error) {
       res.status(400).send({'ERROR': error})
     }
-  } else if(req.method === 'POST') {
-    try {
-      const { name, description, image } = req.body;
-      if(!name || !description || !image) {
-        return res.status(400).json('Missing Data')
-      }
+  // } else if(req.method === 'POST') {
+  //   try {
+  //     const { name, description, image } = req.body;
+  //     if(!name || !description || !image) {
+  //       return res.status(400).json('Missing Data')
+  //     }
 
-      await Empresas.create({
-        name,
-        description,
-        image
-      })
+  //     await Empresas.create({
+  //       name,
+  //       description,
+  //       image
+  //     })
 
-      const created = await Empresas.findOne({where: { name }})
+  //     const created = await Empresas.findOne({where: { name }})
 
-      created
-      ? res.status(200).json({"Created Successfull": created})
-      : res.status(400).json("Error creating the post");
+  //     created
+  //     ? res.status(200).json({"Created Successfull": created})
+  //     : res.status(400).json("Error creating the post");
 
-    } catch (error) {
-      return res.status(500).json(error.message)
-    }
+  //   } catch (error) {
+  //     return res.status(500).json(error.message)
+  //   }
     
-  } else if(req.method === 'PUT') {
-    try {
-      const { id, name, description, image } = req.body;
+  // } else if(req.method === 'PUT') {
+  //   try {
+  //     const { id, name, description, image } = req.body;
 
-      if (!id) {
-        return res.status(400).send("An id is requeried");
-      } else {
-        if (name) {
-          await Empresas.update({ name }, { where: { id } });
-        }
-        if (description) {
-          await Empresas.update({ description }, { where: { id } });
-        }
-        if (image) {
-          await Empresas.update({ image }, { where: { id } });
-        }
-      }
+  //     if (!id) {
+  //       return res.status(400).send("An id is requeried");
+  //     } else {
+  //       if (name) {
+  //         await Empresas.update({ name }, { where: { id } });
+  //       }
+  //       if (description) {
+  //         await Empresas.update({ description }, { where: { id } });
+  //       }
+  //       if (image) {
+  //         await Empresas.update({ image }, { where: { id } });
+  //       }
+  //     }
 
-      const post = await Empresas.findByPk(id)
-      return res.status(200).send(post)
-    } catch (error) {
-      return res.status(500).json(error.message)
-    }
+  //     const post = await Empresas.findByPk(id)
+  //     return res.status(200).send(post)
+  //   } catch (error) {
+  //     return res.status(500).json(error.message)
+  //   }
     
-  } else if(req.method === 'DELETE') {
-    try {
-      const { id } = req.query;
-      if(!id) {
-        return res.status(400).json('Missing Data')
-      }
-      const post = await Empresas.findByPk(id)
-      if(!post) {
-        return res.status(400).json('ID not found')
-      }
-      await Empresas.destroy({ where: { id } })
-      res.status(200).json("Deleted Successfull")
+  // } else if(req.method === 'DELETE') {
+  //   try {
+  //     const { id } = req.query;
+  //     if(!id) {
+  //       return res.status(400).json('Missing Data')
+  //     }
+  //     const post = await Empresas.findByPk(id)
+  //     if(!post) {
+  //       return res.status(400).json('ID not found')
+  //     }
+  //     await Empresas.destroy({ where: { id } })
+  //     res.status(200).json("Deleted Successfull")
 
 
 
-    } catch (error) {
-      return res.status(500).json(error.message)
-    }
+  //   } catch (error) {
+  //     return res.status(500).json(error.message)
+  //   }
   }
 }
